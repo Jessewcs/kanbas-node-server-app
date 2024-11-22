cde17826f4c7c53e240348c84c4acff5c4c67b2a
Jessewcs <jessewojo11@gmail.com>
2024-11-22 17:20:40 -0500

    a5

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
